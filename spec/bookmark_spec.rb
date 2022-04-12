f6e37ea9bacb998bcc7cdd318d537827751558fa
Conor Hope <c_hope@hotmail.co.uk>
2022-04-12 21:01:40 +0100

    Added helper method #setup_test_database

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -9,7 +9,14 @@ describe Bookmark do
 
   context '#all' do
     it 'returns all bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com/');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.destroyallsoftware.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.google.com/');")
+
       bookmarks = Bookmark.all
+    
       expect(bookmarks).to include('http://www.makersacademy.com/')
       expect(bookmarks).to include('http://www.destroyallsoftware.com')
       expect(bookmarks).to include('http://www.google.com/')
---
+++ spec/setup_test_database.rb
@@ -0,0 +1,8 @@
+require 'pg'
+
+puts "Setting up test database..."
+
+connection = PG.connect(dbname: 'bookmark_manager_test')
+
+# Clear the bookmarks table
+connection.exec("TRUNCATE bookmarks;")
