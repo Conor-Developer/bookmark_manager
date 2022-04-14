827410b79aa812afe2e4a46e972de107c32fdf07
Conor Hope <c_hope@hotmail.co.uk>
2022-04-14 13:43:36 +0100

    Added Update bookmark functionality

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -40,4 +40,16 @@ describe Bookmark do
       expect(persisted_data).to eq(nil)
     end
   end
+
+  context '#update' do
+    it 'updates an existing bookmark' do
+      bookmark = Bookmark.create(url: 'http://bbc.co.uk', title: 'BBC')
+      updated_bookmark = Bookmark.update(id: bookmark.id, url: 'http://www.makersacademy.com', title: 'Makers Academy')
+
+      expect(updated_bookmark).to be_a Bookmark
+      expect(updated_bookmark.id).to eq bookmark.id
+      expect(updated_bookmark.url).to eq 'http://www.makersacademy.com'
+      expect(updated_bookmark.title).to eq 'Makers Academy'
+    end
+  end
 end
--- spec/features/update_a_bookmark_spec.rb
+++ spec/features/update_a_bookmark_spec.rb
@@ -1,14 +1,18 @@
 feature 'Update bookmark' do
   it'to update the information of an existing bookmark' do
-    Bookmark.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+    bookmark = Bookmark.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
     visit('/bookmarks')
     expect(page).to have_link('Makers Academy', href: 'http://www.makersacademy.com')
-    first('.bookmark').click_button 'Update'
+    
+    first('.bookmark').click_button 'Edit'
+    expect(current_path).to eq "/bookmarks/#{bookmark.id}/edit"
 
-    expect(current_path).to eq '/bookmarks/update'
     fill_in('title', with: 'BBC')
     fill_in('url', with: 'http://www.bbc.co.uk')
     click_button('Submit')
+
+    expect(current_path).to eq '/bookmarks'
+    expect(page).not_to have_link('Makers Academy', href: 'http://makersacademy.com')
     expect(page).to have_link('BBC', href: 'http://www.bbc.co.uk')
   end
 end
